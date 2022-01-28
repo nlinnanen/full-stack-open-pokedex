134f5bd2fc988d557610e2cde529d46a81da74ed
Niklas Linnanen <nlinnanen@gmail.com>
2022-01-28 13:35:42 +0200

    Add e2e test for page navigation

--- cypress/integration/pokedex_test.js
+++ cypress/integration/pokedex_test.js
@@ -5,4 +5,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Single pokemon page can be navigated', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('Next').click()
+    cy.contains('venusaur')
+  })
 })
